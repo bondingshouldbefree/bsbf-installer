216245dd09c6ed05cd576cb2e0088b97a05acbb9
Chester A. Unal <chester.a.unal@arinc9.com>
2026-02-13 16:19:27 +0200

    Completely switch to xray
    Signed-off-by: Chester A. Unal <chester.a.unal@arinc9.com>

--- bsbf-client-installer.sh
+++ bsbf-client-installer.sh
@@ -37,30 +37,36 @@ done
 
 BSBF_RESOURCES="https://raw.githubusercontent.com/bondingshouldbefree/bsbf-resources/refs/heads/main"
 
-# Install v2ray and its configuration.
-curl -fsSL https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh | sudo bash
-curl -s $BSBF_RESOURCES/resources-client/v2ray.json \
+# Install bash, ethtool, fping, jq, and usb-modeswitch.
+sudo apt update
+sudo apt install bash ethtool fping jq usb-modeswitch
+
+# Install xray and its configuration.
+curl -fsSL https://github.com/XTLS/Xray-install/raw/main/install-release.sh | sudo bash
+curl -s $BSBF_RESOURCES/resources-client/xray.json \
   | jq --arg SERVER "$server_ipv4" \
        --argjson PORT "$server_port" \
        --arg UUID "$uuid" '
-        .outbounds[0].settings.vnext[0].address = $SERVER
-      | .outbounds[0].settings.vnext[0].port = $PORT
-      | .outbounds[0].settings.vnext[0].users[0].id = $UUID' \
-  | sudo tee /usr/local/etc/v2ray/bsbf-bonding.json > /dev/null
+        .outbounds[0].settings.address = $SERVER
+      | .outbounds[0].settings.port = $PORT
+      | .outbounds[0].settings.id = $UUID' \
+  | sudo tee /usr/local/etc/xray/bsbf-bonding.json > /dev/null
 
-# Install ethtool, fping, and usb-modeswitch.
-sudo apt update
-sudo apt install ethtool fping usb-modeswitch
+sudo mkdir /etc/nftables
+sudo curl $BSBF_RESOURCES/resources-client/bsbf_bonding.nft -o /etc/nftables/bsbf_bonding.nft
+sudo curl $BSBF_RESOURCES/resources-client/99-bsbf-bonding.conf -o /etc/systemd/system/xray@.service.d/99-bsbf-bonding.conf
 
-# Install bsbf-mptcp-helper.
-sudo curl $BSBF_RESOURCES/bsbf-mptcp-helper/files/usr/sbin/bsbf-mptcp-backup -o /usr/sbin/bsbf-mptcp-backup
-sudo chmod +x /usr/sbin/bsbf-mptcp-backup
-sudo curl $BSBF_RESOURCES/bsbf-mptcp-helper/files/usr/sbin/bsbf-mptcp-helper -o /usr/sbin/bsbf-mptcp-helper
+# Install bsbf-mptcp.
+sudo curl $BSBF_RESOURCES/bsbf-mptcp/files/usr/sbin/bsbf-mptcp -o /usr/sbin/bsbf-mptcp
+sudo chmod +x /usr/sbin/bsbf-mptcp
+sudo curl $BSBF_RESOURCES/bsbf-mptcp/files/usr/sbin/bsbf-mptcp-helper -o /usr/sbin/bsbf-mptcp-helper
 sudo chmod +x /usr/sbin/bsbf-mptcp-helper
+sudo curl $BSBF_RESOURCES/resources-client/bsbf-mptcp.service -o /usr/lib/systemd/system/bsbf-mptcp.service
 
 # Install bsbf-route.
 sudo curl $BSBF_RESOURCES/bsbf-route/files/usr/sbin/bsbf-route -o /usr/sbin/bsbf-route
 sudo chmod +x /usr/sbin/bsbf-route
+sudo curl $BSBF_RESOURCES/resources-client/bsbf-route.service -o /usr/lib/systemd/system/bsbf-route.service
 
 # Install bsbf-tcp-in-udp.
 sudo mkdir -p /usr/local/share/tcp-in-udp
@@ -74,20 +80,9 @@ sudo chmod +x /usr/sbin/bsbf-tcp-in-udp
 sudo curl $BSBF_RESOURCES/resources-client/99-bsbf-tcp-in-udp.sh -o /etc/NetworkManager/dispatcher.d/99-bsbf-tcp-in-udp.sh
 sudo chmod +x /etc/NetworkManager/dispatcher.d/99-bsbf-tcp-in-udp.sh
 
-# Install systemd services.
-sudo curl $BSBF_RESOURCES/resources-client/bsbf-mptcp-backup.service -o /usr/lib/systemd/system/bsbf-mptcp-backup.service
-sudo curl $BSBF_RESOURCES/resources-client/bsbf-route.service -o /usr/lib/systemd/system/bsbf-route.service
-
 # Enable and (re)start systemd services.
-sudo systemctl enable bsbf-mptcp-backup bsbf-route v2ray@bsbf-bonding
-sudo systemctl restart bsbf-mptcp-backup bsbf-route v2ray@bsbf-bonding
-
-# (Re)install ip rule and route.
-# TODO: These commands must run after v2ray@bsbf-bonding. Make a systemd service?
-# TODO: Move bsbf-bonding.nft to bsbf-resources/resources-client.
-sudo nft -f bsbf-bonding.nft
-sudo ip rule add fwmark 1 table 100 priority 0
-sudo ip route add local default dev lo table 100
+sudo systemctl enable bsbf-mptcp bsbf-route xray@bsbf-bonding
+sudo systemctl restart bsbf-mptcp bsbf-route xray@bsbf-bonding
 
 # Restart NetworkManager to apply the TCP-in-UDP dispatcher script.
 sudo systemctl restart NetworkManager
